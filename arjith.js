959ae06e0145b1430020c2c9e4e5b307f92de984
IAMMNK <mnk111204@gmail.com>
2025-09-18 19:17:18 +0530

    Initial commit

---
+++ arjith.js
@@ -0,0 +1,349 @@
+const music = new Audio("audio/arjith/01.mp3");
+// music.play();
+
+const songs = [
+  {
+    id: 1,
+    songName: `Tum Kya Mile <br />
+        <div class="subtitle">Rocky aur Rani ki Prem Kahani</div>`,
+    poster: "img/arjith/1.webp",
+  },
+  {
+    id: 2,
+    songName: `Channa Mereya <br />
+        <div class="subtitle">Channa</div>`,
+    poster: "img/arjith/2.webp",
+  },
+  {
+    id: 3,
+    songName: `Maya<br />
+        <div class="subtitle">Naa Peru Surya Naa Illu India</div>`,
+    poster: "img/arjith/3.webp",
+  },
+  {
+    id: 4,
+    songName: `Kalank Title Song <br />
+        <div class="subtitle">Kalank</div>`,
+    poster: "img/arjith/4.webp",
+  },
+  {
+    id: 5,
+    songName: `O Maahi...O Maahi <br />
+        <div class="subtitle">Dunki</div>`,
+    poster: "img/arjith/5.webp",
+  },
+  {
+    id: 6,
+    songName: `Lut Put Gaye <br />
+        <div class="subtitle">Dunki</div>`,
+    poster: "img/arjith/6.webp",
+  },
+  {
+    id: 7,
+    songName: `Agar Tum Saath Ho <br />
+        <div class="subtitle">Tamasha</div>`,
+    poster: "img/arjith/7.webp",
+  },
+  {
+    id: 8,
+    songName: `Apna Banale Piya <br />
+        <div class="subtitle">Bhediya</div>`,
+    poster: "img/arjith/8.webp",
+  },
+  {
+    id: 9,
+    songName: `Tera Fitoor <br />
+        <div class="subtitle">Genius</div>`,
+    poster: "img/arjith/9.webp",
+  },
+  {
+    id: 10,
+    songName: `Deva Deva <br />
+        <div class="subtitle">Bramhastra</div>`,
+    poster: "img/arjith/10.webp",
+  },
+  {
+    id: 11,
+    songName: `Kesariya <br />
+        <div class="subtitle">Bramhastra</div>`,
+    poster: "img/arjith/11.webp",
+  },
+  {
+    id: 12,
+    songName: `Ranjha <br />
+        <div class="subtitle">Ranjha</div>`,
+    poster: "img/arjith/12.webp",
+  },
+  {
+    id: 13,
+    songName: `Zaleema <br />
+        <div class="subtitle">Raees</div>`,
+    poster: "img/arjith/13.webp",
+  },
+];
+
+Array.from(document.getElementsByClassName("songItem")).forEach((e, i) => {
+  e.getElementsByTagName("img")[0].src = songs[i].poster;
+  e.getElementsByTagName("h5")[0].innerHTML = songs[i].songName;
+});
+
+let search_results = document.getElementsByClassName("search_results")[0];
+
+songs.forEach((element) => {
+  const { id, songName, poster } = element;
+  console.log(id);
+  console.log(songName);
+  console.log(poster);
+  let card = document.createElement("a");
+  card.classList.add("card");
+  card.href = "#" + id;
+  card.innerHTML = `
+  <img src="${poster}" alt="">
+                        <div class="content">
+                          ${songName}
+                        </div>
+`;
+  search_results.appendChild(card);
+});
+
+let input = document.getElementsByTagName("input")[0];
+
+input.addEventListener("keyup", () => {
+  let input_value = input.value.toUpperCase();
+  let items = search_results.getElementsByTagName("a");
+
+  for (let index = 0; index < items.length; index++) {
+    let as = items[index].getElementsByClassName("content")[0];
+    let text_value = as.textContent || as.innerHTML;
+
+    if (text_value.toUpperCase().indexOf(input_value) > -1) {
+      items[index].style.display = "flex";
+    } else {
+      items[index].style.display = "none";
+    }
+    if (input.value == 0) {
+      search_results.style.display = "none";
+    } else {
+      search_results.style.display = "";
+    }
+  }
+});
+
+let masterPlay = document.getElementById("masterPlay");
+let wave = document.getElementById("wave");
+
+masterPlay.addEventListener("click", () => {
+  if (music.paused || music.currentTime <= 0) {
+    music.play();
+    wave.classList.add("active1");
+    masterPlay.classList.remove("bi-play-fill");
+    masterPlay.classList.add("bi-pause-fill");
+  } else {
+    music.pause();
+    wave.classList.remove("active1");
+    masterPlay.classList.add("bi-play-fill");
+    masterPlay.classList.remove("bi-pause-fill");
+  }
+});
+
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("playlistplay")).forEach((el) => {
+    el.classList.add("bi-play-circle-fill");
+    el.classList.remove("bi-pause-circle-fill");
+  });
+};
+
+const makeAllBackground = () => {
+  Array.from(document.getElementsByClassName("songItem")).forEach((el) => {
+    el.style.background = "rgb(105, 105, 105, .0)";
+  });
+};
+
+let index = 0;
+let poster_master_play = document.getElementById("poster_master_play");
+let download_music = document.getElementById("download_music");
+let title = document.getElementById("title");
+
+Array.from(document.getElementsByClassName("playlistplay")).forEach((e) => {
+  e.addEventListener("click", (el) => {
+    index = el.target.id;
+    // console.log(index);
+    music.src = `audio/arjith/${index}.mp3`;
+    poster_master_play.src = `img/arjith/${index}.webp`;
+    music.play();
+    masterPlay.classList.remove("bi-play-fill");
+    masterPlay.classList.add("bi-pause-fill");
+
+    download_music.href = `audio/arjith/${index}.mp3`;
+
+    let songTitles = songs.filter((els) => {
+      return els.id == index;
+    });
+
+    songTitles.forEach((elss) => {
+      let { songName } = elss;
+      title.innerHTML = songName;
+      download_music.setAttribute("download", songName);
+    });
+
+    makeAllBackground();
+    Array.from(document.getElementsByClassName("songItem"))[
+      index - 1
+    ].style.background = "rgb(105, 105, 105, .1)";
+    makeAllPlays();
+    el.target.classList.remove("bi-play-circle-fill");
+    el.target.classList.add("bi-pause-circle-fill");
+    wave.classList.add("active1");
+  });
+});
+
+let currentStart = document.getElementById("currentStart");
+let currentEnd = document.getElementById("currentEnd");
+let seek = document.getElementById("seek");
+let bar2 = document.getElementById("bar2");
+let dot = document.getElementsByClassName("dot")[0];
+
+music.addEventListener("timeupdate", () => {
+  let music_curr = music.currentTime;
+  let music_dur = music.duration;
+  // console.log(music_curr);
+  let min1 = Math.floor(music_dur / 60);
+  let sec1 = Math.floor(music_dur % 60);
+
+  // console.log(min1);
+  // console.log(sec1);
+  if (sec1 < 10) {
+    sec1 = `0${sec1}`;
+  }
+  currentEnd.innerText = `${min1}:${sec1}`;
+
+  let min2 = Math.floor(music_curr / 60);
+  let sec2 = Math.floor(music_curr % 60);
+
+  if (sec2 < 10) {
+    sec2 = `0${sec2}`;
+  }
+
+  currentStart.innerText = `${min2}:${sec2}`;
+
+  let progressBar = parseInt((music_curr / music_dur) * 100);
+  seek.value = progressBar;
+  // console.log(seek.value);
+  let seekbar = seek.value;
+  bar2.style.width = `${seekbar}%`;
+  dot.style.left = `${seekbar}%`;
+});
+
+seek.addEventListener("change", () => {
+  music.currentTime = (seek.value * music.duration) / 100;
+});
+
+let vol_icon = document.getElementById("vol_icon");
+let vol = document.getElementById("vol");
+let vol_bar = document.getElementsByClassName("vol_bar")[0];
+let vol_dot = document.getElementById("vol_dot");
+
+vol.addEventListener("change", () => {
+  if (vol.value == 0) {
+    vol_icon.classList.remove("bi-volume-up-fill");
+    vol_icon.classList.remove("bi-volume-down-fill");
+    vol_icon.classList.add("bi-volume-off-fill");
+  }
+  if (vol.value > 0) {
+    vol_icon.classList.remove("bi-volume-up-fill");
+    vol_icon.classList.add("bi-volume-down-fill");
+    vol_icon.classList.remove("bi-volume-off-fill");
+  }
+  if (vol.value > 50) {
+    vol_icon.classList.add("bi-volume-up-fill");
+    vol_icon.classList.remove("bi-volume-down-fill");
+    vol_icon.classList.remove("bi-volume-off-fill");
+  }
+  let vol_a = vol.value;
+  vol_bar.style.width = `${vol_a}%`;
+  vol_dot.style.left = `${vol_a}%`;
+  music.volume = vol_a / 100;
+});
+
+let back = document.getElementById("back");
+let next = document.getElementById("next");
+
+back.addEventListener("click", () => {
+  index -= 1;
+  if (index < 1) {
+    index = Array.from(document.getElementsByClassName("songItem")).length;
+  }
+  music.src = `audio/arjith/${index}.mp3`;
+  poster_master_play.src = `img/arjith/${index}.webp`;
+  music.play();
+  masterPlay.classList.remove("bi-play-fill");
+  masterPlay.classList.add("bi-pause-fill");
+
+  let songTitles = songs.filter((els) => {
+    return els.id == index;
+  });
+
+  songTitles.forEach((elss) => {
+    let { songName } = elss;
+    title.innerHTML = songName;
+  });
+  makeAllBackground();
+  Array.from(document.getElementsByClassName("songItem"))[
+    index - 1
+  ].style.background = "rgb(105, 105, 105, .1)";
+  makeAllPlays();
+  el.target.classList.remove("bi-play-circle-fill");
+  el.target.classList.add("bi-pause-circle-fill");
+  wave.classList.add("active1");
+});
+
+next.addEventListener("click", () => {
+  index++;
+  if (index > Array.from(document.getElementsByClassName("songItem")).length) {
+    index = 1;
+  }
+  music.src = `audio/arjith/${index}.mp3`;
+  poster_master_play.src = `img/arjith/${index}.webp`;
+  music.play();
+  masterPlay.classList.remove("bi-play-fill");
+  masterPlay.classList.add("bi-pause-fill");
+
+  let songTitles = songs.filter((els) => {
+    return els.id == index;
+  });
+
+  songTitles.forEach((elss) => {
+    let { songName } = elss;
+    title.innerHTML = songName;
+  });
+  makeAllBackground();
+  Array.from(document.getElementsByClassName("songItem"))[
+    index - 1
+  ].style.background = "rgb(105, 105, 105, .1)";
+  makeAllPlays();
+  el.target.classList.remove("bi-play-circle-fill");
+  el.target.classList.add("bi-pause-circle-fill");
+  wave.classList.add("active1");
+});
+
+let pop_song_left = document.getElementById("pop_song_left");
+let pop_song_right = document.getElementById("pop_song_right");
+let pop_song = document.getElementsByClassName("pop_song")[0];
+
+pop_song_right.addEventListener("click", () => {
+  pop_song.scrollLeft += 330;
+});
+pop_song_left.addEventListener("click", () => {
+  pop_song.scrollLeft -= 330;
+});
+
+let pop_art_left = document.getElementById("pop_art_left");
+let pop_art_right = document.getElementById("pop_art_right");
+let item = document.getElementsByClassName("item")[0];
+
+pop_art_right.addEventListener("click", () => {
+  item.scrollLeft += 330;
+});
+pop_art_left.addEventListener("click", () => {
+  item.scrollLeft -= 330;
+});
